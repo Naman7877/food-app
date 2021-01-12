7df1bb31793cd9134b7f2f8fc37b3da0c429527e
naman khandelwal <64783908+Naman7877@users.noreply.github.com>
2021-01-12 22:59:34 +0530

    i am put

---
+++ app/src/main/java/com/example/foodies/splash.java
@@ -0,0 +1,44 @@
+package com.example.foodies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
+        Thread thread=new Thread()
+        {
+            public void run()
+            {
+                try {
+                    sleep(2000);
+
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+
+                }
+                finally {
+                    Intent intent= new Intent(splash.this,MainActivity.class);
+                    startActivity(intent);
+                }
+
+
+            }
+        };thread.start();
+
+
+    }
+}
